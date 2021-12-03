f3b2a6e8ac4f0985fb9678ba9afbc018d622c40e
soulmachine <soulmachine@gmail.com>
2021-12-03 02:00:34 -0800

    Merge data instead of overwrite

--- src/main.rs
+++ src/main.rs
@@ -28,12 +28,29 @@ fn main() {
         for market_type in market_types {
             println!("{} {}", exchange, market_type);
             if let Ok(mut markets) = crypto_markets::fetch_markets(exchange, market_type) {
+                let file_path =
+                    Path::new("./data").join(format!("{}.{}.json", exchange, market_type));
+                if file_path.exists() {
+                    // merge data
+                    let text = std::fs::read_to_string(file_path.as_path()).unwrap();
+                    let mut existed_markets =
+                        serde_json::from_str::<Vec<crypto_markets::Market>>(&text)
+                            .unwrap()
+                            .into_iter()
+                            .filter(|old_market| {
+                                !markets
+                                    .iter()
+                                    .any(|new_market| new_market.symbol == old_market.symbol)
+                            })
+                            .collect::<Vec<crypto_markets::Market>>();
+                    markets.append(&mut existed_markets);
+                }
                 markets.sort_by_key(|x| x.symbol.clone());
                 let f_out = std::fs::OpenOptions::new()
                     .create(true)
                     .write(true)
                     .truncate(true)
-                    .open(Path::new("./data").join(format!("{}.{}.json", exchange, market_type)))
+                    .open(file_path.as_path())
                     .unwrap();
                 let mut buf_writer = std::io::BufWriter::new(f_out);
                 for market in markets.iter_mut() {
